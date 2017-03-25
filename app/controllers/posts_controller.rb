18f1f6b38adcf4fef1f34eea444b88cc4aae0fdc
williamjones62 <williamjones62@gmail.com>
2017-03-25 16:13:22 -0400

    Add Post resource and DB table

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,64 @@
 class PostsController < ApplicationController
+
+  before_action :set_post
+
+  def post_data
+    post = post.find(params[:id])
+    render json: post.to_json
+  end
+
+  def create
+    @post = @posts.build(posts_params)
+    if @post.save
+      render :json => @post
+    else
+      render json: { errors: "Error creating post, please try again."}
+    end
+  end
+
+  def index
+    render :json => @posts
+  end
+
+  def show
+    @post = post.find(params[:id])
+    if @post
+      render json: @post
+    else
+      render json: { errors: "This is not a post, please try again."}
+    end
+  end
+
+  def update
+    @post = post.find(params[:id])
+    if @post.update(posts_params)
+      render nothing: true
+    else
+      render json: { errors: "Error updating post, please try again."}
+    end
+  end
+
+  def destroy
+    @post = post.find(params[:id])
+    if @post.destroy
+      render nothing: true
+    else
+      render json: { errors: "Error deleting post, please try again"}
+    end
+  end
+
+  def data
+    post = post.find(params[:id])
+    render json: post.to_json(only: [:title, :body])
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    def posts_params
+      params.require(:post).permit(:title, :body, :league_id, :team_id, :game_id)
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323204326) do
+ActiveRecord::Schema.define(version: 20170325014928) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "league_id"
+    t.integer  "team_id"
+    t.integer  "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
